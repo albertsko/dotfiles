7063de820a534c644f80e944837468ceed56e17b
Albert Skonieczny <albertskonieczny@gmail.com>
2025-03-15 16:06:51 +0100

    feat: prepare initial cfg

---
+++ .install.sh
@@ -0,0 +1,125 @@
+#!/bin/zsh
+
+# Install xCode cli tools
+echo "Installing commandline tools..."
+xcode-select --install
+
+# Homebrew
+## Install
+echo "Installing Brew..."
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew analytics off
+
+## Taps
+echo "Tapping Brew..."
+brew tap FelixKratz/formulae
+brew tap koekeishiya/formulae
+
+## Formulae
+echo "Installing Brew Formulae..."
+brew install wget
+brew install jq
+brew install ripgrep
+brew install mas
+brew install gh
+brew install ifstat
+brew install switchaudio-osx
+brew install skhd
+brew install borders
+brew install yabai
+brew install rclone
+
+### Terminal
+brew install neovim
+brew install starship
+brew install zsh-autosuggestions
+brew install zsh-fast-syntax-highlighting
+brew install zoxide
+
+### Containers
+brew install docker
+brew install colima
+brew install minikube
+brew install k9s
+brew install lazydocker
+
+### Nice to have
+brew install htop
+brew install lazygit
+brew install nnn
+
+## Casks
+echo "Installing Brew Casks..."
+brew install --cask ghostty
+brew install --cask google-chrome
+brew install --cask zen-browser
+brew install --cask zoom
+brew install --cask slack
+brew install --cask vlc
+brew install --cask spotify
+
+### Fonts
+brew install --cask sf-symbols
+brew install --cask font-sf-mono
+brew install --cask font-sf-pro
+brew install --cask font-hack-nerd-font
+brew install --cask font-jetbrains-mono-nerd-font
+
+# macOS Settings
+echo "Changing macOS defaults..."
+defaults write com.apple.NetworkBrowser BrowseAllInterfaces 1 # enable browsing of all network interfaces
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true # prevent creation of .ds_store files on network drives
+defaults write com.apple.spaces spans-displays -bool false # disable spaces spanning multiple displays
+defaults write com.apple.dock autohide -bool true # enable dock auto-hide
+defaults write com.apple.dock autohide-delay -float 0 # start showing dock immediatly
+defaults write com.apple.dock autohide-time-modifier -float 0.5 # show whole dock in 0.5s
+defaults write com.apple.dock "mru-spaces" -bool "false" # disable automatically rearranging spaces based on recent use
+defaults write NSGlobalDomain NSAutomaticWindowAnimationsEnabled -bool false # disable window opening animations
+defaults write com.apple.LaunchServices LSQuarantine -bool false # disable quarantine prompt for downloaded apps
+# defaults write NSGlobalDomain com.apple.swipescrolldirection -bool false # disable natural scrolling
+defaults write NSGlobalDomain KeyRepeat -int 1 # set keyboard repeat rate to fast
+defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false # disable automatic spelling correction
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true # show all file extensions in finder
+defaults write NSGlobalDomain _HIHideMenuBar -bool false # do not hide the menu bar by default
+# defaults write NSGlobalDomain AppleHighlightColor -string "0.65098 0.85490 0.58431" # set highlight color to a custom green
+defaults write NSGlobalDomain AppleAccentColor -int 1 # set accent color to orange
+defaults write com.apple.screencapture location -string "$HOME/Desktop" # set screenshot save location to desktop
+defaults write com.apple.screencapture disable-shadow -bool true # disable screenshot shadow
+defaults write com.apple.screencapture type -string "png" # set screenshot format to png
+defaults write com.apple.finder DisableAllAnimations -bool true # disable finder animations
+defaults write com.apple.finder ShowExternalHardDrivesOnDesktop -bool false # hide external hard drives from desktop
+defaults write com.apple.finder ShowHardDrivesOnDesktop -bool false # hide internal hard drives from desktop
+defaults write com.apple.finder ShowMountedServersOnDesktop -bool false # hide mounted servers from desktop
+defaults write com.apple.finder ShowRemovableMediaOnDesktop -bool false # hide removable media from desktop
+defaults write com.apple.Finder AppleShowAllFiles -bool true # show hidden files in finder
+defaults write com.apple.finder FXDefaultSearchScope -string "SCcf" # set finder search scope to current folder
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false # disable warning when changing file extensions
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true # show full path in finder title
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv" # set finder view style to list view
+defaults write com.apple.finder ShowStatusBar -bool false # hide status bar in finder
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool YES # prevent time machine from offering new disks for backup
+defaults write com.apple.mail AddressesIncludeNameOnPasteboard -bool false # disable copying email addresses with names in mail app
+# defaults write -g NSWindowShouldDragOnGesture YES # allow dragging windows with three-finger gesture
+sudo defaults write com.apple.Safari AutoOpenSafeDownloads -bool false # disable safari auto-opening safe downloads
+sudo defaults write com.apple.Safari IncludeDevelopMenu -bool true
+sudo defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+sudo defaults write com.apple.Safari com.apple.Safari.ContentPageGroupIdentifier.WebKit2DeveloperExtrasEnabled -bool true
+sudo defaults write NSGlobalDomain WebKitDeveloperExtras -bool true
+killall Finder Dock SystemUIServer # apply most of the macOS defaults changes
+
+## Fix for Bluetooth devices while using Wi-Fi
+sudo defaults write /Library/Preferences/com.apple.airport.bt.plist bluetoothCoexMgmt Hybrid
+
+## Copying and checking out configuration files
+echo "Planting Configuration Files..."
+[ ! -d "$HOME/dotfiles" ] && git clone --bare git@github.com:albsko/dotfiles.git $HOME/dotfiles
+git --git-dir=$HOME/dotfiles/ --work-tree=$HOME checkout main
+
+source $HOME/.zshrc
+cfg config --local status.showUntrackedFiles no
+
+## Start Services
+echo "Starting Services (grant permissions)..."
+brew services start skhd
+brew services start fyabai
+brew services start borders
--- .zshrc
+++ .zshrc
@@ -1,104 +1,74 @@
-# If you come from bash you might have to change your $PATH.
-# export PATH=$HOME/bin:$HOME/.local/bin:/usr/local/bin:$PATH
+# Source zsh plugins
+source $(brew --prefix)/share/zsh-fast-syntax-highlighting/fast-syntax-highlighting.plugin.zsh
+source $(brew --prefix)/share/zsh-autosuggestions/zsh-autosuggestions.zsh
 
-# Path to your Oh My Zsh installation.
-export ZSH="$HOME/.oh-my-zsh"
+# Aliases for common dirs
+alias home="cd ~"
 
-# Set name of the theme to load --- if set to "random", it will
-# load a random theme each time Oh My Zsh is loaded, in which case,
-# to know which specific one was loaded, run: echo $RANDOM_THEME
-# See https://github.com/ohmyzsh/ohmyzsh/wiki/Themes
-ZSH_THEME="robbyrussell"
+# System Aliases
+alias ..="cd .."
+alias x="exit"
 
-# Set list of themes to pick from when loading at random
-# Setting this variable when ZSH_THEME=random will cause zsh to load
-# a theme from this variable instead of looking in $ZSH/themes/
-# If set to an empty array, this variable will have no effect.
-# ZSH_THEME_RANDOM_CANDIDATES=( "robbyrussell" "agnoster" )
+# Git Aliases
+alias add="git add"
+alias commit="git commit"
+alias pull="git pull"
+alias stat="git status"
+alias gdiff="git diff HEAD"
+alias vdiff="git difftool HEAD"
+alias log="git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+alias cfg="git --git-dir=$HOME/dotfiles/ --work-tree=$HOME"
+alias push="git push"
+alias g="lazygit"
 
-# Uncomment the following line to use case-sensitive completion.
-# CASE_SENSITIVE="true"
+eval "$(starship init zsh)"
+eval "$(zoxide init --cmd cd zsh)"
 
-# Uncomment the following line to use hyphen-insensitive completion.
-# Case-sensitive completion must be off. _ and - will be interchangeable.
-# HYPHEN_INSENSITIVE="true"
+alias ssh="TERM=xterm-256color ssh"
 
-# Uncomment one of the following lines to change the auto-update behavior
-# zstyle ':omz:update' mode disabled  # disable automatic updates
-# zstyle ':omz:update' mode auto      # update automatically without asking
-# zstyle ':omz:update' mode reminder  # just remind me to update when it's time
+alias n="nnn"
+function nnn () {
+  command nnn "$@"
 
-# Uncomment the following line to change how often to auto-update (in days).
-# zstyle ':omz:update' frequency 13
+  if [ -f "$NNN_TMPFILE" ]; then
+          . "$NNN_TMPFILE"
+  fi
+}
 
-# Uncomment the following line if pasting URLs and other text is messed up.
-# DISABLE_MAGIC_FUNCTIONS="true"
+function kill () {
+  command kill -KILL $(pidof "$@")
+}
 
-# Uncomment the following line to disable colors in ls.
-# DISABLE_LS_COLORS="true"
+function suyabai () {
+  SHA256=$(shasum -a 256 /opt/homebrew/bin/yabai | awk "{print \$1;}")
+  if [ -f "/private/etc/sudoers.d/yabai" ]; then
+    sudo sed -i '' -e 's/sha256:[[:alnum:]]*/sha256:'${SHA256}'/' /private/etc/sudoers.d/yabai
+  else
+    echo "sudoers file does not exist yet"
+  fi
+}
 
-# Uncomment the following line to disable auto-setting terminal title.
-# DISABLE_AUTO_TITLE="true"
+# Color Scheme
+export BLACK=0xff181819
+export WHITE=0xffe2e2e3
+export RED=0xfffc5d7c
+export GREEN=0xff9ed072
+export BLUE=0xff76cce0
+export YELLOW=0xffe7c664
+export ORANGE=0xfff39660
+export MAGENTA=0xffb39df3
+export GREY=0xff7f8490
+export TRANSPARENT=0x00000000
+export BG0=0xff2c2e34
+export BG1=0xff363944
+export BG2=0xff414550
 
-# Uncomment the following line to enable command auto-correction.
-# ENABLE_CORRECTION="true"
+export NNN_TMPFILE="$HOME/.config/nnn/.lastd"
+export NNN_OPTS="AdHoU"
+# export NNN_FCOLORS='c1e2272e006033f7c6d6abc4'
 
-# Uncomment the following line to display red dots whilst waiting for completion.
-# You can also set it to another string to have that shown instead of the default red dots.
-# e.g. COMPLETION_WAITING_DOTS="%F{yellow}waiting...%f"
-# Caution: this setting can cause issues with multiline prompts in zsh < 5.7.1 (see #5765)
-# COMPLETION_WAITING_DOTS="true"
+export EDITOR="$(which zed)"
+export VISUAL="$(which zed)"
+export MANPAGER="$(which nvim) +Man!"
 
-# Uncomment the following line if you want to disable marking untracked files
-# under VCS as dirty. This makes repository status check for large repositories
-# much, much faster.
-# DISABLE_UNTRACKED_FILES_DIRTY="true"
-
-# Uncomment the following line if you want to change the command execution time
-# stamp shown in the history command output.
-# You can set one of the optional three formats:
-# "mm/dd/yyyy"|"dd.mm.yyyy"|"yyyy-mm-dd"
-# or set a custom format using the strftime function format specifications,
-# see 'man strftime' for details.
-# HIST_STAMPS="mm/dd/yyyy"
-
-# Would you like to use another custom folder than $ZSH/custom?
-# ZSH_CUSTOM=/path/to/new-custom-folder
-
-# Which plugins would you like to load?
-# Standard plugins can be found in $ZSH/plugins/
-# Custom plugins may be added to $ZSH_CUSTOM/plugins/
-# Example format: plugins=(rails git textmate ruby lighthouse)
-# Add wisely, as too many plugins slow down shell startup.
-plugins=(git)
-
-source $ZSH/oh-my-zsh.sh
-
-# User configuration
-
-# export MANPATH="/usr/local/man:$MANPATH"
-
-# You may need to manually set your language environment
-# export LANG=en_US.UTF-8
-
-# Preferred editor for local and remote sessions
-# if [[ -n $SSH_CONNECTION ]]; then
-#   export EDITOR='vim'
-# else
-#   export EDITOR='nvim'
-# fi
-
-# Compilation flags
-# export ARCHFLAGS="-arch $(uname -m)"
-
-# Set personal aliases, overriding those provided by Oh My Zsh libs,
-# plugins, and themes. Aliases can be placed here, though Oh My Zsh
-# users are encouraged to define aliases within a top-level file in
-# the $ZSH_CUSTOM folder, with .zsh extension. Examples:
-# - $ZSH_CUSTOM/aliases.zsh
-# - $ZSH_CUSTOM/macos.zsh
-# For a full list of active aliases, run `alias`.
-#
-# Example aliases
-# alias zshconfig="mate ~/.zshrc"
-# alias ohmyzsh="mate ~/.oh-my-zsh"
+export XDG_CONFIG_HOME="$HOME/.config"
