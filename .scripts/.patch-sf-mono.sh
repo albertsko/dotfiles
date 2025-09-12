5e11e20c20f4d011bca949058560c9f439f053bd
Albert Skonieczny <askonieczny@fandom.com>
2025-09-12 16:41:46 +0200

    feat(fonts): patch SF Mono script

---
+++ .scripts/.patch-sf-mono.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -e
+
+INPUT_DIR="/tmp/SFMonoUnpatched"
+OUTPUT_DIR="$HOME/Library/Fonts"
+FONT_PATH="/Library/Fonts/SF-Mono*.otf"
+
+mkdir -p "$INPUT_DIR"
+
+for file in $FONT_PATH; do
+    echo "Copying $file..."
+    cp "$file" "$INPUT_DIR/"
+done
+
+docker run --rm \
+    --user "$(id -u):$(id -g)" \
+    -v "$INPUT_DIR":/in \
+    -v "$OUTPUT_DIR":/out \
+    nerdfonts/patcher \
+    --removeligs \
+    --complete
+
+rm -rf "$INPUT_DIR"
