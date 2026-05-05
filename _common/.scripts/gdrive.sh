3d07549fc24f1bf87b09592c9825d86dff7e968d
Albert Skonieczny <50720306+albertsko@users.noreply.github.com>
2026-05-05 19:44:57 +0200

    refactor: gdrive.sh with gum

--- _common/.scripts/gdrive.sh
+++ _common/.scripts/gdrive.sh
@@ -2,13 +2,13 @@
 set -euo pipefail
 
 readonly REMOTE='gdrive:'
-readonly STALE_THRESHOLD_DAYS=7
 readonly FILTERS_FILE="${HOME}/.config/rclone/filters.txt"
 
 readonly LOCAL_DIR="${HOME}/rclone-gdrive"
+mkdir -p "$LOCAL_DIR"
+
 readonly RCLONE_TEST_FILE='RCLONE_TEST'
-readonly SUCCESS_MARKER="${LOCAL_DIR}/${RCLONE_TEST_FILE}"
-mkdir -p "${LOCAL_DIR}"
+readonly RCLONE_TEST_FILE_PATH="${LOCAL_DIR}/${RCLONE_TEST_FILE}"
 
 readonly -a BISYNC_FLAGS=(
 	--create-empty-src-dirs
@@ -32,28 +32,54 @@ readonly -a BISYNC_FLAGS=(
 
 cmd=(rclone bisync "${REMOTE}" "${LOCAL_DIR}" "${BISYNC_FLAGS[@]}" "$@")
 
-needs_resync='false'
-if fd -q --older "${STALE_THRESHOLD_DAYS}d" -t f -g "$(basename "${SUCCESS_MARKER}")" "$(dirname "${SUCCESS_MARKER}")" --exact-depth 1; then
-	needs_resync='true'
+if ! test -f "${RCLONE_TEST_FILE_PATH}"; then
+	touch "${RCLONE_TEST_FILE_PATH}"
 fi
-
-if [[ ! -f "${SUCCESS_MARKER}" ]]; then
-	needs_resync='true'
-	touch "${SUCCESS_MARKER}"
+if ! rclone lsf "${REMOTE}${RCLONE_TEST_FILE}" >/dev/null 2>&1; then
 	rclone touch "${REMOTE}${RCLONE_TEST_FILE}"
 fi
 
-if [[ "${needs_resync}" == 'true' ]]; then
-	echo "Resyncing..."
-	cmd+=('--resync' '--resync-mode' 'newer')
+has_force=0
+tmp_stderr=$(mktemp)
+"${cmd[@]}" 2>"${tmp_stderr}" || true
+
+stderr_output=$(<"${tmp_stderr}")
+rm -f "${tmp_stderr}"
+
+if [[ "${stderr_output}" == *"--force"* ]]; then
+	"${cmd[@]}" --dry-run || true
+	has_force=1
 fi
 
-if ! "${cmd[@]}"; then
-	rm -f "${SUCCESS_MARKER}"
+if ! [[ "${has_force}" -eq 1 ]]; then
+	test -n "${stderr_output}" && echo "${stderr_output}" >&2
+	echo "yay!"
+	exit 0
+fi
+
+BOLD=$'\033[1m'
+RESET=$'\033[0m'
+gum_header="
+Bisync requires manual resolution. Select an option:
+
+${BOLD}1)${RESET} --force
+${BOLD}2)${RESET} --resync-mode newer
+${BOLD}3)${RESET} --resync-mode older
+${BOLD}4)${RESET} --resync-mode path1
+${BOLD}5)${RESET} --resync-mode path2
+"
+
+choice=$(gum input --header "${gum_header}" --placeholder "1-5" --char-limit 1 --header.foreground="212")
+case "${choice}" in
+1) "${cmd[@]}" --force ;;
+2) "${cmd[@]}" --resync --resync-mode newer ;;
+3) "${cmd[@]}" --resync --resync-mode older ;;
+4) "${cmd[@]}" --resync --resync-mode path1 ;;
+5) "${cmd[@]}" --resync --resync-mode path2 ;;
+*)
+	echo "${BOLD}Invalid key clicked. Aborting.${RESET}" >&2
 	exit 1
-fi
+	;;
+esac
 
-touch "${SUCCESS_MARKER}"
-rclone touch "${REMOTE}${RCLONE_TEST_FILE}"
-
-echo "Success."
+echo "yay!"
