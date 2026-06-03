215ab7f38d3ac1923bd08c2413a291df25d27ad0
Albert Skonieczny <50720306+albertsko@users.noreply.github.com>
2026-06-03 13:59:38 +0200

    example commit just like this

--- shared/.config/fish/config.fish
+++ shared/.config/fish/config.fish
@@ -24,6 +24,7 @@ function log
     git log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit $argv
 end
 
+
 function ggg
     git add -A && git commit -m "$argv" && git push
 end
