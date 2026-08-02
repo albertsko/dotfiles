8d6e7eabeb5da88b9069a4703b81376b7730dd82
Albert Skonieczny <50720306+albertsko@users.noreply.github.com>
2026-08-02 14:11:51 +0200

    chore(vault): add macos wiring

--- macos/_install.sh
+++ macos/_install.sh
@@ -3,6 +3,14 @@ set -euo pipefail
 
 SCRIPT_DIR="$(dirname -- "$(realpath -- "${BASH_SOURCE[0]}")")"
 
+VAULT_GIT_DIR="${VAULT_GIT_DIR:-${XDG_DATA_HOME:-$HOME/.local/share}/.vault.git}"
+readonly VAULT_REPO_URL="git@github.com:albertsko/vault.git"
+
+if [[ ! -d "$VAULT_GIT_DIR" ]]; then
+	git clone --bare "$VAULT_REPO_URL" "$VAULT_GIT_DIR"
+	git --git-dir="$VAULT_GIT_DIR" read-tree HEAD
+fi
+
 bash "$SCRIPT_DIR/.local/bin/sudo-touchid.sh"
 bash "$SCRIPT_DIR/.local/bin/brew.sh"
 bash "$SCRIPT_DIR/.local/bin/macos.sh"
